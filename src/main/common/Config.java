7aec85f9700a456c104bc14d9a68ce837e349de7
jxh <544468820@qq.com>
2026-09-22 14:10:49 +0800

    修改如下报错： 1.引用公网js文件bootstrap.min.js失败，改为本地 2.引用公网js文件jquery.min.js失败，改为本地 3.引用公网css文件bootstrap.min.css失败，改为本地 4.添加fonts字体文件，解决日历控件图标显示错误 5.修改Config.java的mian方法，解决本地调试报错找不到静态文件 6.重新打包发布release版本

--- src/main/common/Config.java
+++ src/main/common/Config.java
@@ -13,28 +13,39 @@ import com.jfinal.server.undertow.UndertowServer;
 
 /**
  * @desc
- * @author	JuanWoo
- * @date	2015年11月28日
+ * @author JuanWoo
+ * @date 2015年11月28日
  **/
 
 public class Config extends JFinalConfig {
-	public void configConstant(Constants me) {
-		me.setDevMode(true);
-	}
+    public void configConstant(Constants me) {
 
-	public void configRoute(Routes me) {
-		me.add("/", StoryController.class, "/story");
-	}
+        me.setDevMode(true);
+    }
 
-	public void configPlugin(Plugins me) {}
+    public void configRoute(Routes me) {
+        me.add("/", StoryController.class, "story");
+    }
 
-	public void configInterceptor(Interceptors me) {}
+    public void configPlugin(Plugins me) {
+    }
 
-	public void configHandler(Handlers me) {
-		me.add(new ContextPathHandler("base"));
-	}
+    public void configInterceptor(Interceptors me) {
+    }
+
+    public void configHandler(Handlers me) {
+
+        me.add(new ContextPathHandler("base"));
+    }
 
     public static void main(String[] args) {
-        UndertowServer.start(Config.class, 80, true);
+        // 本项目的 Web 资源目录名为 Web，不在 jfinal-undertow 的默认探测列表
+        // (src/main/webapp, WebRoot, WebContent, webapp) 内，必须显式指定；
+        // 否则 ServletContext.getRealPath(...) 返回 null，Freemarker 找不到 /story/*.html
+        UndertowServer.create(Config.class)
+                .setPort(80)
+                .setDevMode(true)
+                .setResourcePath("Web")
+                .start();
     }
 }
